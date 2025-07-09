429ea957e5fd8050e6cdb803decb4d08c4de735f
henriquehiga <codehiga@gmail.com>
2025-07-09 18:06:40 -0300

    feat: resgatando informações do dataset

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-def main():
-    print("Hello from previsao-risco-credito!")
+import pandas as pd
 
+# Dataset
+url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/statlog/german/german.data'
 
-if __name__ == "__main__":
-    main()
+# Nomes de coluna conforme doc: https://archive.ics.uci.edu/dataset/144/statlog+german+credit+data
+colunas = [
+    'status_conta_corrente', 'duracao_mes', 'historico_credito', 'proposito',
+    'valor_credito', 'poupanca', 'tempo_emprego_atual', 'taxa_prestacao',
+    'estado_civil_sexo', 'outros_devedores', 'residencia_desde', 'propriedade',
+    'idade', 'outros_planos_pagamento', 'habitacao', 'num_creditos_existentes',
+    'emprego', 'num_dependentes', 'telefone', 'trabalhador_estrangeiro', 'risco'
+]
+
+df_credito = pd.read_csv(url, sep=' ', header=None, names=colunas)
